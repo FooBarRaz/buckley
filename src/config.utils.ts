0881f04e1dd94c026e1ea0de258fa86e55c79217
Raz Ali <mehraz.hakim@gmail.com>
2024-04-22 23:16:47 -0400

    add support for yaml configs

--- src/config.utils.ts
+++ src/config.utils.ts
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import path from 'path';
+import yaml from 'js-yaml';
+
 
 type Platforms = "darwin" | "win32" | "linux";
 
@@ -21,6 +23,15 @@ export type Configuration = {
 export function parseConfigFile(filePath: string): Configuration {
   const absolutePath = path.resolve(filePath);
   const fileContents = fs.readFileSync(absolutePath, 'utf-8');
-  const config: Configuration = JSON.parse(fileContents);
+
+  let config: Configuration;
+  if (path.extname(filePath) === '.json') {
+    config = JSON.parse(fileContents);
+  } else if (path.extname(filePath) === '.yaml' || path.extname(filePath) === '.yml') {
+    config = yaml.load(fileContents) as Configuration;
+  } else {
+    throw new Error(`Unsupported file type: ${filePath}`);
+  }
+
   return config;
 }
